c385994b8b4178f16c4df86bfc52bf5bf71a6651
aashman-thakur <70641344+aashman-thakur@users.noreply.github.com>
2020-09-25 18:17:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,68 @@
-const Engine=Matter.Engine
-const World=Matter.World
-const Bodies=Matter.Bodies
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine,world,ball,g;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
 function setup() {
-  createCanvas(800,400);
-  engine=Engine.create()
-  world=engine.world
-  var re={
-    'restitution':1.0,
-   ' density':0.8,
-    'friction':0.8
-  }
-  ball=Bodies.circle(200,200,50,re)
-  World.add(world,ball)
-  var go={
-    isStatic:true
-  }
-  g=Bodies.rectangle(400,375,800,50,go)
-  World.add(world,g)
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,50);
+	groundSprite.shapeColor=color('green')
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.rectangle(width/2 , 200 , 50 , 50,{restitution:1.2,density:8,});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+	
 }
 
+
 function draw() {
-  background(255,255,150); 
-  Engine.update(engine)
-  ellipseMode(RADIUS)
-  ellipse(ball.position.x,ball.position.y,50);
-  rectMode(CENTER)
-  rect(g.position.x,g.position.y,800,50);
+	Engine.update(engine)
+	
+  rectMode(CENTER);
+  background(rgb(105,206,235));
+  
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
 }
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+    
+  }
+}
+
+
+
