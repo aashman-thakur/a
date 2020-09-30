5d5f4d043eddae9cb019b065e7cd2971b31a3866
aashman-thakur <70641344+aashman-thakur@users.noreply.github.com>
2020-09-30 17:43:00 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -1,24 +1,27 @@
-class Box{
-    constructor(x,y,width,height){
-    var options={
-        'restitution':1,
-        'density':0.8,
-        'friction':0.3
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':3,
+        'density':1.0
     }
-this.body=Bodies.rectangle(x,y,width,height,options)
-this.width=width;
-this.height=height;
-World.add(world,this.body)
-    }
-    display(){
-        var p=this.body.position
-        var angle=this.body.angle;
-        push()
-        translate(p.x,p.y)
-        rotate(angle)
-        rectMode(CENTER)
-        console.log(angle)
-        rect(0,0,this.width,this.height)
-        pop()
-    }
-}
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    noFill();
+    strokeWeight(10)
+    stroke('brown')
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
--- sketch.js
+++ sketch.js
@@ -3,23 +3,41 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1,box2,g,box3;
+var box1;
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-   box1=new Box(500,100,50,100) 
-   box2=new Box(600,200,50,50)
-  g=new Ground(200,375,width,30)
-  box3=new Box(470,10,50,50)
+    ground = new Ground(600,height,width,20)
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    box5=new Box(810,160,70,70)
+    pig1=new Pig(810,350,50,50)
+    pig2=new Pig(810,220,50,50)
+    log3=new Log(760,120,30,150,PI/7)
+    log4=new Log(870,120,30,150,PI/-7)
+    log1=new Log(810,270,30,300,PI/2)
+    log2=new Log(810,180,30,300,PI/2)
+    bird1=new Bird(100,100)
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
-    box1.display()
-    box2.display()
-    g.display()
-    box3.display()
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display()
+    log1.display()
+    box3.display();
+    box4.display();
+    log2.display();
+    pig2.display()
+    box5.display()
+    log3.display()
+    log4.display()
+    bird1.display()
 }
